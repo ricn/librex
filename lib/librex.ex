48fa81956a20b9860240969a5a37e729ac5494ec
Richard Nyström <ricn@rny.io>
2015-03-16 22:35:47 +0100

    Added a convert! function thats raise error and the convert function now return {:ok, out_file} or {:error, reason} to follow Elixir API conventions

--- lib/librex.ex
+++ lib/librex.ex
@@ -1,11 +1,20 @@
 defmodule Librex do
 
-
   def convert(in_file, out_file) do
-    unless File.exists?(in_file) do
-      raise File.Error, reason: :enoent, action: "read", path: in_file
+    case File.stat(in_file) do
+      {:ok, _} -> do_convert(in_file, out_file)
+      {:error, reason} -> { :error, reason }
     end
+  end
 
+  def convert!(in_file, out_file) do
+    case convert(in_file, out_file) do
+      {:ok, _}      -> out_file
+      {:error, reason} -> raise File.Error, reason: reason, action: "read", path: in_file
+    end
+  end
+
+  defp do_convert(in_file, out_file) do
     basename = Path.basename(in_file, Path.extname(in_file))
     convert_to = String.replace(Path.extname(out_file), ".", "")
     out_temp_dir = System.tmp_dir! <> "/" <> SecureRandom.uuid <> "/"
@@ -16,6 +25,8 @@ defmodule Librex do
     File.cp! out_temp_file, out_file
     File.rm! out_temp_file
     File.rmdir! out_temp_dir
+
+    { :ok, out_file }
   end
 
   defp run(file_to_convert, out_dir, convert_to) do
--- test/librex_test.exs
+++ test/librex_test.exs
@@ -5,31 +5,45 @@ defmodule LibrexTest do
   @pptx_file Path.join(__DIR__, "fixtures/pptx.pptx")
   @non_existent_file Path.join(__DIR__, "fixtures/non.existent")
 
-  test "convert docx to pdf" do
+  test ".convert docx to pdf" do
     pdf_file = random_path <> ".pdf"
     refute File.exists? pdf_file
-    Librex.convert(@docx_file, pdf_file)
+    { :ok, out_file } = Librex.convert(@docx_file, pdf_file)
+    assert pdf_file == out_file
     assert is_pdf? pdf_file
   end
 
-  test "convert pptx to pdf" do
+  test ".convert pptx to pdf" do
     pdf_file = random_path <> ".pdf"
     refute File.exists? pdf_file
-    Librex.convert(@pptx_file, pdf_file)
+    { :ok, out_file } = Librex.convert(@pptx_file, pdf_file)
+    assert pdf_file == out_file
     assert is_pdf? pdf_file
   end
 
-  test "convert docx to odt" do
+  test ".convert docx to odt" do
     odt_file = random_path <> ".odt"
     refute File.exists? odt_file
-    Librex.convert(@docx_file, odt_file)
+    { :ok, out_file } = Librex.convert(@docx_file, odt_file)
+    assert odt_file == out_file
     assert is_odt? odt_file
   end
 
-  test "raise error when file to convert does not exist" do
+  test ".convert must return :error when file to convert does not exist" do
+    { :error, reason } = Librex.convert(@non_existent_file, "/tmp/output.pdf")
+    assert reason == :enoent
+  end
+
+  test ".convert! must return output file path" do
+    pdf_file = random_path <> ".pdf"
+    out_file = Librex.convert!(@docx_file, pdf_file)
+    assert pdf_file == out_file
+  end
+
+  test ".convert! raise error when file to convert does not exist" do
     msg = "could not read #{@non_existent_file}: no such file or directory"
     assert_raise File.Error, msg, fn ->
-      Librex.convert(@non_existent_file, "/tmp/output.pdf")
+      Librex.convert!(@non_existent_file, "/tmp/output.pdf")
     end
   end
 
