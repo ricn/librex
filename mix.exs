7c191323ef165d66b4efa7031f269580958b563b
Richard Nyström <ricn@rny.io>
2020-07-07 23:37:53 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Librex.Mixfile do
   def project do
     [
       app: :librex,
-      version: "1.0.3",
+      version: "1.0.4",
       elixir: "~> 1.0",
       description: description(),
       package: package(),
